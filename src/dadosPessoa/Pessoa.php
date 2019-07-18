549d49b698c4ebf0deba4ed7a5a79cfd847d53a2
Felipe Nogueira <contato.fnog@gmail.com>
2019-07-18 09:08:17 -0300

    Add tests and assertions to check age and email

--- src/dadosPessoa/Pessoa.php
+++ src/dadosPessoa/Pessoa.php
@@ -5,14 +5,36 @@ namespace MiniaturePancake\dadosPessoa;
 class Pessoa
 {
     private $nome;
+    private $idade;
+    private $email;
 
     public function atribuirNome($nome)
     {
       $this->nome = $nome;
     }
 
+    public function atribuirIdade($idade)
+    {
+      $this->idade = $idade;
+    }
+
+    public function atribuirEmail($email)
+    {
+      return $this->email = $email;
+    }
+
     public function exibirNome()
     {
       return $this->nome;
+    }
+
+    public function exibirIdade()
+    {
+      return $this->idade;
+    }
+
+    public function exibirEmail()
+    {
+      return $this->email;
     }
 }
--- tests/dadosPessoa/PessoaTest.php
+++ tests/dadosPessoa/PessoaTest.php
@@ -7,8 +7,34 @@ class PessoaTest extends TestCase
 {
     public function testVerificarNome()
     {
+        $pNome = 'João';
+
         $dados = new Pessoa();
-        $dados->atribuirNome('João');
-        $this->assertEquals('João', $dados->exibirNome());
+        $dados->atribuirNome($pNome);
+        $this->assertEquals($pNome, $dados->exibirNome());
+        $this->assertTrue(is_string($dados->exibirNome()));
+    }
+
+    public function testVerificarIdade()
+    {
+        $pIdade = 22;
+
+        $dados = new Pessoa();
+        $dados->atribuirIdade($pIdade);
+        $this->assertEquals($pIdade, $dados->exibirIdade());
+        $this->assertTrue(is_integer($dados->exibirIdade()));
+    }
+
+    public function testVerificarEmail()
+    {
+        $pEmail = 'example@example.com';
+
+        $dados = new Pessoa();
+        $dados->atribuirEmail($pEmail);
+        $this->assertEquals($pEmail, $dados->exibirEmail());
+        $this->assertEquals($pEmail, $dados->exibirEmail());
+        $this->assertEquals(
+            $pEmail, filter_var($dados->exibirEmail(), FILTER_VALIDATE_EMAIL) 
+        );
     }
 }
